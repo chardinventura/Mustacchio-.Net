b7dddda2ad72342d9d35287b0c0d3b11628dae91
Chardin De La Cruz Ventura <chardindelacruzventura829@gmail.com>
2019-11-09 23:31:22 -0400

    version 1

---
+++ Mustacchio/Mustacchio/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mustacchio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Nosotros()
+        {
+            return View();
+        }
+
+        public ActionResult Galeria()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            return View();
+        }
+
+        public ActionResult Contacto()
+        {
+            return View();
+        }
+    }
+}
